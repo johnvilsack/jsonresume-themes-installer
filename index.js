018e02cdacf2a507a78c0a9d512410b34fc349f3
John Vilsack <vilsack@gmail.com>
2022-05-12 17:30:01 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const THEMES = require('jsonresume-themes-feed').getThemes()
+//const themes = require('jsonresume-themes-feed').getThemes()
+
+
+const fs = require('fs')
+const list = JSON.parse(fs.readFileSync('themes.json', 'utf8'))
+const {fork} = require('child_process')
+//const list = JSON.parse(fs.readFileSync('themes.json', 'utf8'))
+
+async function main() {
+  Object.entries(list).forEach(([key, theme]) => {
+    const childProcess = fork('./worker.js')
+    childProcess.send(theme)
+    childProcess.on("message", message => {
+      console.log("Received Message")
+    })
+  })
+} main()
+
---
+++ worker.js
@@ -0,0 +1,17 @@
+const { execSync } = require('child_process')
+
+process.on("message", theme => {
+  console.log(theme.name)
+  const result = installTheme(theme)
+  process.send(result)
+  process.exit()
+})
+
+
+async function installTheme(theme) {
+  execSync('npm install ' + theme.name)
+  console.log(theme.name)
+  return {
+    installTheme: installTheme
+  }
+}
